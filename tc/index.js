d771d03c9a39e64ff8c318d48db952b02b31b24f
Ian Channing <ian.channing@imec.be>
2020-05-31 22:38:43 +0200

    feat: combine with html form inputs, handle bad inputs, display message

--- tc/index.js
+++ tc/index.js
@@ -1,3 +1,34 @@
+const raw = {
+  fields: ['Mission', 'Snek', 'Zloy', 'Dutch', 'Diana', 'Charon', 'Varg', 'David', 'Syndrome', 'Joe', 'Valera', 'Capisce', 'Klaus', 'Shi', 'Victor', 'Spencer', 'Travis', 'Batya', 'Hawk', 'Jason', 'Boris', 'Thor', 'Rick', 'Mishka', 'Rookie'],
+  data: [
+    ['B.S.S.', 200, 200, 200, 200, 200, 255, 95, 95, 95, 95, 95, 70, 70, 70, 190, 70, 70, 60, 160, 60, 60, 60, 60, 50],
+    ['Basic Mission', 200, 200, 200, 200, 200, 160, 160, 160, 160, 160, 160, 120, 120, 120, 120, 120, 120, 100, 100, 100, 100, 100, 100, 80],
+    ['Bayonet', 200, 200, 200, 200, 200, 65, 210, 65, 65, 65, 65, 155, 155, 155, 155, 155, 50, 40, 150, 150, 150, 150, 40, 35],
+    ['Breach', 200, 200, 200, 200, 200, 115, 115, 240, 115, 115, 115, 85, 85, 180, 85, 85, 85, 70, 70, 150, 70, 70, 70, 55],
+    ['Cleanup', 200, 200, 200, 200, 200, 115, 115, 115, 240, 115, 115, 85, 85, 85, 180, 85, 85, 70, 70, 70, 150, 70, 70, 55],
+    ['Common Only', 200, 200, 200, 200, 200, 65, 65, 65, 65, 65, 65, 50, 50, 50, 50, 50, 50, 150, 150, 150, 150, 150, 150, 120],
+    ['Cover', 200, 200, 200, 200, 200, 115, 115, 115, 115, 240, 115, 85, 85, 85, 85, 180, 85, 70, 70, 70, 70, 150, 70, 55],
+    ['Hammer', 200, 200, 200, 200, 200, 95, 95, 95, 95, 95, 95, 240, 70, 70, 70, 70, 240, 60, 60, 190, 60, 190, 190, 50],
+    ['HILDR', 200, 200, 200, 200, 200, 480, 100, 100, 100, 100, 100, 70, 70, 70, 70, 70, 70, 60, 60, 60, 300, 60, 60, 50],
+    ['Knife', 200, 200, 200, 200, 200, 90, 90, 90, 90, 320, 90, 65, 240, 240, 65, 240, 65, 55, 55, 55, 55, 55, 55, 45],
+    ['Locals', 200, 200, 200, 200, 200, 80, 80, 225, 225, 80, 225, 60, 60, 60, 60, 60, 170, 140, 50, 50, 50, 50, 140, 40],
+    ['Logistics', 200, 200, 200, 200, 200, 115, 240, 115, 115, 115, 115, 85, 180, 85, 85, 85, 85, 70, 150, 70, 70, 70, 70, 55],
+    ['Rare Only', 200, 200, 200, 200, 200, 240, 240, 240, 240, 240, 240, 50, 50, 50, 50, 50, 50, 40, 40, 40, 40, 40, 40, 30],
+    ['Recon', 200, 200, 200, 200, 200, 240, 115, 115, 115, 115, 115, 180, 85, 85, 85, 85, 85, 150, 70, 70, 70, 70, 70, 55],
+    ['Showdown', 200, 200, 200, 200, 200, 115, 115, 115, 115, 115, 240, 85, 85, 85, 85, 85, 180, 70, 70, 70, 70, 70, 150, 55],
+    ['Uncommon Only', 200, 200, 200, 200, 200, 65, 65, 65, 65, 65, 65, 180, 180, 180, 180, 180, 180, 40, 40, 40, 40, 40, 40, 30],
+  ],
+};
+
+
+// handle spaces and ignore case
+// also need to handle duplicates and misspellings
+const clean = (list) => (list.map((x) => (x.trim())).filter((x) => (x)));
+
+const check = (cleanList) => (cleanList.filter((x) => (
+  !raw.data.find((y) => (y[0].toUpperCase() === x.toUpperCase()))
+)));
+
 /**
  * Mission op placement
  *
@@ -6,28 +37,6 @@
  * @returns {Array} Missions rows with names of each operator for each mission
  */
 const missions = (names, completedNames = []) => {
-  const raw = {
-    fields: ['Mission', 'Snek', 'Zloy', 'Dutch', 'Diana', 'Charon', 'Varg', 'David', 'Syndrome', 'Joe', 'Valera', 'Capisce', 'Klaus', 'Shi', 'Victor', 'Spencer', 'Travis', 'Batya', 'Hawk', 'Jason', 'Boris', 'Thor', 'Rick', 'Mishka', 'Rookie'],
-    data: [
-      ['B.S.S.', 200, 200, 200, 200, 200, 255, 95, 95, 95, 95, 95, 70, 70, 70, 190, 70, 70, 60, 160, 60, 60, 60, 60, 50],
-      ['Basic Mission', 200, 200, 200, 200, 200, 160, 160, 160, 160, 160, 160, 120, 120, 120, 120, 120, 120, 100, 100, 100, 100, 100, 100, 80],
-      ['Bayonet', 200, 200, 200, 200, 200, 65, 210, 65, 65, 65, 65, 155, 155, 155, 155, 155, 50, 40, 150, 150, 150, 150, 40, 35],
-      ['Breach', 200, 200, 200, 200, 200, 115, 115, 240, 115, 115, 115, 85, 85, 180, 85, 85, 85, 70, 70, 150, 70, 70, 70, 55],
-      ['Cleanup', 200, 200, 200, 200, 200, 115, 115, 115, 240, 115, 115, 85, 85, 85, 180, 85, 85, 70, 70, 70, 150, 70, 70, 55],
-      ['Common Only', 200, 200, 200, 200, 200, 65, 65, 65, 65, 65, 65, 50, 50, 50, 50, 50, 50, 150, 150, 150, 150, 150, 150, 120],
-      ['Cover', 200, 200, 200, 200, 200, 115, 115, 115, 115, 240, 115, 85, 85, 85, 85, 180, 85, 70, 70, 70, 70, 150, 70, 55],
-      ['Hammer', 200, 200, 200, 200, 200, 95, 95, 95, 95, 95, 95, 240, 70, 70, 70, 70, 240, 60, 60, 190, 60, 190, 190, 50],
-      ['HILDR', 200, 200, 200, 200, 200, 480, 100, 100, 100, 100, 100, 70, 70, 70, 70, 70, 70, 60, 60, 60, 300, 60, 60, 50],
-      ['Knife', 200, 200, 200, 200, 200, 90, 90, 90, 90, 320, 90, 65, 240, 240, 65, 240, 65, 55, 55, 55, 55, 55, 55, 45],
-      ['Locals', 200, 200, 200, 200, 200, 80, 80, 225, 225, 80, 225, 60, 60, 60, 60, 60, 170, 140, 50, 50, 50, 50, 140, 40],
-      ['Logistics', 200, 200, 200, 200, 200, 115, 240, 115, 115, 115, 115, 85, 180, 85, 85, 85, 85, 70, 150, 70, 70, 70, 70, 55],
-      ['Rare Only', 200, 200, 200, 200, 200, 240, 240, 240, 240, 240, 240, 50, 50, 50, 50, 50, 50, 40, 40, 40, 40, 40, 40, 30],
-      ['Recon', 200, 200, 200, 200, 200, 240, 115, 115, 115, 115, 115, 180, 85, 85, 85, 85, 85, 150, 70, 70, 70, 70, 70, 55],
-      ['Showdown', 200, 200, 200, 200, 200, 115, 115, 115, 115, 115, 240, 85, 85, 85, 85, 85, 180, 70, 70, 70, 70, 70, 150, 55],
-      ['Uncommon Only', 200, 200, 200, 200, 200, 65, 65, 65, 65, 65, 65, 180, 180, 180, 180, 180, 180, 40, 40, 40, 40, 40, 40, 30],
-    ],
-  };
-
   /**
    * Helper function that ignores first column
    *
@@ -45,15 +54,19 @@ const missions = (names, completedNames = []) => {
    * @param {[string]} complete Completed mission names
    * @returns {Array} Missions rows with values for each operator for each mission
    */
-  const calcBase = (all, complete) => (
-    // use the all array to maintain its ordering
-    all
-      .map((x) => (raw.data.find((y) => y[0] === x)))
+  const calcBase = (all, complete) => {
+    const upperComplete = complete.map((x) => (x.toUpperCase()));
+    // map over the all array to maintain its ordering
+    return all
+      // match ignoring case
+      .map((x) => (raw.data.find((y) => y[0].toUpperCase() === x.toUpperCase())))
       .map((x) => (
-        // zero completed rows
-        complete.includes(x[0]) ? x.map((y, i) => (first(y, i, 0))) : x
-      ))
-  );
+        // set completed rows to zero
+        upperComplete.includes(x[0].toUpperCase())
+          ? x.map((y, i) => (first(y, i, 0)))
+          : x
+      ));
+  };
 
   /**
    * Get the max for each operator of the current missions
@@ -153,18 +166,41 @@ const missions = (names, completedNames = []) => {
    * @returns {Array} Mission rows with string operator names
    */
   const calc = (completed) => {
-    const base = calcBase(names, completed);
+    const base = calcBase(clean(names), clean(completed));
     const rawOps = calcRawOps(base, calcMax(base));
     const ops = calcOps(rawOps);
     return calcOpNames(ops);
   };
 
   // compare current and previous
-  return calcChanges(calc(completedNames), calc(completedNames.slice(0, -1)));
+  const checkNames = check(clean(names));
+  const checkCompleted = check(clean(completedNames));
+  if (checkNames.length || checkCompleted.length) {
+    return `Couldn't find these mission names: ${[...checkNames, ...checkCompleted].join()}`;
+  }
+  return calcChanges(
+    calc(completedNames),
+    calc(completedNames.length ? completedNames.slice(0, -1) : completedNames),
+  );
+};
+
+const message = (rawMessage, completedNames) => {
+  const upperComplete = clean(completedNames).map((x) => (x.toUpperCase()));
+  const missionMessage = isArray(rawMessage) && rawMessage.reduce((acc, x, i) => {
+    const [head, ...tail] = x;
+    const isComplete = upperComplete.includes(head.toUpperCase());
+    return `${acc}
+
+Mission ${i + 1} (${head}): ${isComplete ? 'Completed' : tail.filter((y) => (y)).join(' ')}`;
+  }, '');
+
+  return `${missionMessage}
+
+Please hit the :white_check_mark: if you are complying`;
 };
 
 // example usage
-console.log(missions(
-  ['Hammer', 'Recon', 'Uncommon Only', 'Bayonet', 'Knife', 'Logistics'],
-  ['Hammer', 'Recon', 'Logistics', 'Uncommon Only'],
-));
+// console.log(missions(
+//   ['Hammer', 'Recon', 'Uncommon Only', 'Bayonet', 'Knife', 'Logistics'],
+//   ['Hammer', 'Recon', 'Logistics', 'Uncommon Only'],
+// ));
