e583d35ef7cebe013fe0ef2fb6e5337382763fa8
Ian Channing <ian@charp.be>
2025-05-16 21:57:03 +0200

    fix: replace some jquery

--- pomo/js/jquery.main.js
+++ pomo/js/jquery.main.js
@@ -1,28 +1,28 @@
 // the dogiest ever example of mixing jQuery  and ES6...
 // the jQuery will disappear...
-import { chuck } from './chuck.js';
+import { chuck } from "./chuck.js";
 (function ($) {
   $.fn.chker = function () {
     var timer = $(this);
     var timerId = 0;
     var tickInterval = 1000;
-    var appName = 'Lock Stock Pomodoros';
-    var siteName = 'ianchanning';
+    var appName = "Lock Stock Pomodoros";
+    var siteName = "ianchanning";
 
-    var mm = document.querySelector('.min');
-    var ss = document.querySelector('.sec');
-    var pp = document.querySelector('.pomo');
-    var start = timer.find('.start');
-    var stop = timer.find('.stop');
+    var mm = document.querySelector(".min");
+    var ss = document.querySelector(".sec");
+    var pp = document.querySelector(".pomo");
+    var start = timer.find(".start");
+    var stop = timer.find(".stop");
 
     var files = [
-      'bacon.txt',
-      'eddie.txt',
-      'soap.txt',
-      'tom.txt',
-      'rory_breaker.txt',
+      "bacon.txt",
+      "eddie.txt",
+      "soap.txt",
+      "tom.txt",
+      "rory_breaker.txt",
     ];
-    var alarm = document.getElementById('alarm');
+    var alarm = document.getElementById("alarm");
 
     var queryString = function () {
       return window.location.search;
@@ -34,53 +34,53 @@ import { chuck } from './chuck.js';
     };
 
     var quotesFile = function () {
-      var i = param('says');
+      var i = param("says");
       var defaultIndex = Math.floor(Math.random() * files.length);
       return files[parseInt(i !== null ? i : defaultIndex)];
     };
 
     var shouldPlaySound = function () {
-      return !['true', '1', ''].includes(param('silent'));
+      return !["true", "1", ""].includes(param("silent"));
     };
 
     // :scream: Validate your inputs son...
     var loadQuotes = function (fileName) {
       // 1. Check for null or undefined
       if (!fileName) {
-        console.error('Filename is missing.');
+        console.error("Filename is missing.");
         return [];
       }
 
       // 2. Check for potentially malicious characters (Defense in Depth)
       const forbiddenCharacters = /[^\w\-.]/; // Allow only alphanumeric, dash, underscore, and dot
       if (forbiddenCharacters.test(fileName)) {
-        console.error('Invalid characters in filename.');
+        console.error("Invalid characters in filename.");
         return [];
       }
 
       // 3. Check for ".." sequences (Defense in Depth)
-      if (fileName.includes('..')) {
-        console.error('Directory traversal attempt detected.');
+      if (fileName.includes("..")) {
+        console.error("Directory traversal attempt detected.");
         return [];
       }
 
       // 4. Check file extension (Defense in Depth)
-      if (!fileName.endsWith('.txt')) {
-        console.error('Invalid file extension.  Only .txt files are allowed.');
+      if (!fileName.endsWith(".txt")) {
+        console.error("Invalid file extension.  Only .txt files are allowed.");
         return [];
       }
 
       // If all checks pass (Defense in Depth - Client Side. SERVER SIDE is key)
-      $.get('quotes/' + fileName, function (data) {
+      $.get("quotes/" + fileName, function (data) {
         try {
-          return Hjson.parse('[' + data + ']');
+          return Hjson.parse("[" + data + "]");
         } catch (error) {
-          console.error('Error parsing Hjson:', error);
+          console.error("Error parsing Hjson:", error);
           // Handle the parsing error gracefully (e.g., display an error message)
           return [];
         }
       }).fail(function (_, textStatus, errorThrown) {
-        console.error('get failed:', textStatus, errorThrown);
+        console.error("get failed:", textStatus, errorThrown);
         // Handle the AJAX error (e.g., display an error message)
         return [];
       });
@@ -89,10 +89,10 @@ import { chuck } from './chuck.js';
     var fetchQuotes = function (fileName) {
       return new Promise((resolve, reject) => {
         // :scream: Validate your inputs son...
-        $.get('quotes/' + fileName, function (data) {
+        $.get("quotes/" + fileName, function (data) {
           try {
             // Assuming data is newline separated strings
-            const quotes = Hjson.parse('[' + data + ']');
+            const quotes = Hjson.parse("[" + data + "]");
             resolve(quotes);
           } catch (error) {
             reject(error);
@@ -101,7 +101,7 @@ import { chuck } from './chuck.js';
       });
     };
 
-    $(".tabs a[href*='says=" + param('says') + "']").addClass('active');
+    $(".tabs a[href*='says=" + param("says") + "']").addClass("active");
 
     Notification.requestPermission();
 
@@ -114,7 +114,7 @@ import { chuck } from './chuck.js';
       // prevent spamming the link
       if (timerId === 0) {
         timerId = setInterval(updateTimer, tickInterval);
-        timer.addClass('ticking');
+        timer.addClass("ticking");
       }
     };
 
@@ -124,7 +124,7 @@ import { chuck } from './chuck.js';
     var stopTimer = function () {
       clearInterval(timerId);
       timerId = 0;
-      timer.removeClass('ticking');
+      timer.removeClass("ticking");
     };
 
     start.click(startTimer);
@@ -134,11 +134,11 @@ import { chuck } from './chuck.js';
      * display the timer in the title
      */
     var updateTitle = function () {
-      var timerText = Array.from(document.querySelector('.time').childNodes)
-        .filter((node) => node.tagName !== 'A')
+      var timerText = Array.from(document.querySelector(".time").childNodes)
+        .filter((node) => node.tagName !== "A")
         .map((node) => node.value || node.textContent.trim())
-        .join(' ');
-      document.title = timerText + ' - ' + appName + ' : ' + siteName;
+        .join(" ");
+      document.title = timerText + " - " + appName + " : " + siteName;
     };
 
     /**
@@ -176,14 +176,14 @@ import { chuck } from './chuck.js';
      * @return {string}
      */
     var ucwords = function (str) {
-      return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+      return (str + "").replace(/^([a-z])|\s+([a-z])/g, function ($1) {
         return $1.toUpperCase();
       });
     };
 
     var quoter = function (fileName) {
-      var file = fileName.split('.');
-      return ucwords(file[0].replace(/_/g, ' '));
+      var file = fileName.split(".");
+      return ucwords(file[0].replace(/_/g, " "));
     };
 
     var randomNotification = function () {
@@ -204,15 +204,21 @@ import { chuck } from './chuck.js';
             body: randomQuote,
           };
 
-          var n = new Notification(quoter(fileName) + ' says', options);
+          var n = new Notification(quoter(fileName) + " says", options);
           setTimeout(n.close.bind(n), 10000);
 
-          $('.notifications').prepend(
-            formatQuote(randomQuote, quoter(fileName))
-          );
+          const notificationsElement = document.querySelector(".notifications");
+          if (notificationsElement) {
+            notificationsElement.insertAdjacentHTML(
+              "afterbegin",
+              formatQuote(randomQuote, quoter(fileName))
+            );
+          } else {
+            console.error('Element with class "notifications" not found.');
+          }
         })
         .catch((error) => {
-          console.error('Failed to fetch quotes:', error);
+          console.error("Failed to fetch quotes:", error);
         });
     };
 
@@ -224,22 +230,22 @@ import { chuck } from './chuck.js';
      */
     var formatQuote = function (quote, speaker) {
       // format single quotes nicely
-      var prettyQuote = quote.replace(/'/g, '&rsquo;');
+      var prettyQuote = quote.replace(/'/g, "&rsquo;");
 
       var now = new Date();
       // Options for a nicely formatted time: 12-hour format with hours, minutes
-      var timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
-      var formattedTime = now.toLocaleTimeString('en-US', timeOptions);
+      var timeOptions = { hour: "numeric", minute: "numeric", hour12: true };
+      var formattedTime = now.toLocaleTimeString("en-US", timeOptions);
 
       // Append the time to the speaker name, wrapped in a span for easy styling
       return (
-        '<blockquote>&ldquo;' +
+        "<blockquote>&ldquo;" +
         prettyQuote +
-        '&rdquo;<figcaption>&mdash; says ' +
+        "&rdquo;<figcaption>&mdash; says " +
         speaker +
-        ' <cite>@ ' +
+        " <cite>@ " +
         formattedTime +
-        '</cite></figcaption></blockquote>'
+        "</cite></figcaption></blockquote>"
       );
     };
 
@@ -250,43 +256,43 @@ import { chuck } from './chuck.js';
     };
   };
 
-  $('#chker').chker();
+  $("#chker").chker();
 
   var checkToggle = function (stopButton, startButton) {
-    return $('#chker').hasClass('ticking') ? stopButton : startButton;
+    return $("#chker").hasClass("ticking") ? stopButton : startButton;
   };
 
   var myToggle = function (myButton) {
-    myButton.click().focus().toggleClass('button-activated');
+    myButton.click().focus().toggleClass("button-activated");
   };
 
   var myActive = function (myButton) {
-    myButton.toggleClass('button-activated');
+    myButton.toggleClass("button-activated");
   };
 
   $(document).keydown(function (event) {
-    if (event.keyCode == '32') {
+    if (event.keyCode == "32") {
       // space bar
       event.preventDefault(); // the default being scroll the page down
-      myActive(checkToggle($('#chker .stop'), $('#chker .start')));
+      myActive(checkToggle($("#chker .stop"), $("#chker .start")));
     }
   });
 
   $(document).keyup(function (event) {
-    if (event.keyCode == '32') {
+    if (event.keyCode == "32") {
       // space bar
       event.preventDefault();
-      myToggle(checkToggle($('#chker .stop'), $('#chker .start')));
+      myToggle(checkToggle($("#chker .stop"), $("#chker .start")));
     }
   });
 
-  $('#reminder').click(function () {
-    if ($(this).text() === '+') {
-      $('.reminder').fadeIn('slow');
-      $(this).html('&times;');
+  $("#reminder").click(function () {
+    if ($(this).text() === "+") {
+      $(".reminder").fadeIn("slow");
+      $(this).html("&times;");
     } else {
-      $('.reminder').fadeOut('slow');
-      $(this).html('+');
+      $(".reminder").fadeOut("slow");
+      $(this).html("+");
     }
   });
 })(jQuery);
